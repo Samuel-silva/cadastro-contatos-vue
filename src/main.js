f227348af71d2cf8848e7064773b455139446f89
Samuel Barbosa <samuca192silva@hotmail.com>
2023-04-19 14:52:23 -0300

    feat - Install vuex

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from './store'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -14,5 +15,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
