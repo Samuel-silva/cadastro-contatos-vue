b0b7e44b6ca9084345fba2a53181e6231f42eacb
Samuel Barbosa <samuca192silva@hotmail.com>
2023-04-24 11:32:57 -0300

    feat - Changes to work with external JSON Mock

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,8 +1,9 @@
 import api from '@/api'
+const key = '2d1a8187-4905-4fab-9f04-988ba9c7b059';
 
 export default {
-  getAllContacts: () => api.get('contacts'),
-  newContact: (payload) => api.post('contacts', payload),
-  deleteContact: (id) => api.delete(`contacts/${id}`),
-  editContact: (id, payload) => api.put(`contacts/${id}`, payload),
+  getAllContacts: () => api.get(key),
+  newContact: (payload) => api.post(key, payload),
+  deleteContact: (id) => api.delete(`${key}/${id}`),
+  editContact: (id, payload) => api.put(`${key}/${id}`, payload),
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:3000/"
+const baseURL = "https://run.mocky.io/v3/"
 
 const api = axios.create({
 	baseURL,
