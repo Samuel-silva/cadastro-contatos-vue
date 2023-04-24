6ad9542140de4bcf4c883ccf496a05d095fe4d8e
Samuel Barbosa <samuca192silva@hotmail.com>
2023-04-24 15:23:22 -0300

    feat - Error for  put, delete and post

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,9 +1,10 @@
 import api from '@/api'
-const key = '2d1a8187-4905-4fab-9f04-988ba9c7b059';
+const keySuccess = '2d1a8187-4905-4fab-9f04-988ba9c7b059';
+const keyError = '84639b56-2779-48a9-8cd9-632baa9b52ff';
 
 export default {
-  getAllContacts: () => api.get(key),
-  newContact: (payload) => api.post(key, payload),
-  deleteContact: (id) => api.delete(`${key}/${id}`),
-  editContact: (id, payload) => api.put(`${key}/${id}`, payload),
+  getAllContacts: () => api.get(keySuccess),
+  newContact: (payload) => api.post(keyError, payload),
+  deleteContact: (id) => api.delete(`${keyError}/${id}`),
+  editContact: (id, payload) => api.put(`${keyError}/${id}`, payload),
 }
